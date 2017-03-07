c952bda2da745a00afa415b7634e1e3f61d0b150
Shane Peelar <lookatyouhacker@gmail.com>
2017-03-06 23:57:08 -0500

    Association list reading

--- PageRank/PageRank.cpp
+++ PageRank/PageRank.cpp
@@ -7,17 +7,47 @@
 //For AVX2 instructions
 #define EIGEN_MAX_ALIGN_BYTES 32
 
-#include "stdafx.h"
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 
+#include <fstream>
 #include <iostream>
 #include <string>
 
 Eigen::Matrix2Xi parse_association_list(const std::string& al_path)
 {
+	std::ifstream file{ al_path };
+	file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
+	//Determine length of file -- each line is an edge, so this can be used to size matrix accordingly 
+	int n_edges = 0;
+	char buffer[1000];
+	while (file.getline(buffer, 1000))
+	{
+		n_edges++;
+	}
+
+	//Start from beginning of file again
+	file.seekg(0);
+
+	Eigen::Matrix2Xi al{ 2, n_edges };
+	//Add edges into AL matrix
+
+	int32_t index = 0;
+	std::string v1;
+	std::string v2;
+
+	while (file >> v1 >> v2)
+	{
+		int paper1 = std::stoi(v1);
+		int paper2 = std::stoi(v2);
+		al(0, index) = paper1;
+		al(1, index) = paper2;
+		index++;
+	}
+
+	return al;
 }
 
 namespace dense
@@ -73,7 +103,7 @@ ComputationType parseComputationArg(const std::string& arg)
 	}
 }
 
-int main(int argc, char*argv[])
+int __cdecl main(int argc, char*argv[])
 {
 
 	if (argc < 3)
